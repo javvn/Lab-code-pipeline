f7ef91cb3308ea3a555e827b204cd7475900b7d1
javvn <juho0428@naver.com>
2023-01-24 19:13:59 +0900

    create repo

--- pipeline/codecommit.tf
+++ pipeline/codecommit.tf
@@ -1,4 +1,8 @@
 resource "aws_codecommit_repository" "dev" {
-  repository_name = "lab-codepipeline"
+  count = 2
+
+  repository_name = "lab-codepipeline${count.index}"
   description     = "The repository for Lab code pipeline"
+
+  tags = local.common_tags
 }
